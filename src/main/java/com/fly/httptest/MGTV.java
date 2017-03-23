895759e0fa9d26611c9484df226ae7ca39d5d2ba
daifei <fei.dai@dmall.com>
2017-03-23 20:11:11 +0800

    方法修改

--- src/main/java/com/fly/httptest/MGTV.java
+++ src/main/java/com/fly/httptest/MGTV.java
@@ -18,57 +18,95 @@ import java.util.Random;
 public class MGTV {
 
     public static void main(String[] args) throws Exception {
-        /*login();
+        login();
 
         share();
 
         wheelGame(10);
 
-        for (int i=0;i<10;i++) {
+        submitHuaFei(5);
+        if (isRedBagCanPlay("10237")) {
+            Thread.sleep(1000);
+            redBag2(3);
+        }
+
+        Thread.sleep(1000*60*60*5+sleepMillisecond(1000, 10000));
+        login();
+        redBag1(5, "10477"); // 每日红包
+
+        if (isRedBagCanPlay("10393")) {
+            Thread.sleep(1000);
+            redBag3(3);
+        }
+
+        /*for (int i=0;i<10;i++) {
             JSONObject totalJson = HttpClientUtils.getResponse("http://activity.mgtvhd.com/gameWebM/GameQuestionOperateTotal_dealQuestion.do?userId=141255&deviceNumber=&gameId=10447&payTypeUser=dou", null);
             String totalId = totalJson.getString("data").substring(1);
             JSONObject jsonObject = HttpClientUtils.getResponse("http://activity.mgtvhd.com/gameWebM/GameQuestionOperateTotal_updateAnswerRecord.do?question=295_492,290_473,310_540,316_556,505_1246,509_1257,515_1274,527_1305,729_1986,733_1994&totalId=" + totalId + "&gameId=10447&userId=141255", null);
             System.out.println(jsonObject);
             Thread.sleep(sleepMillisecond(1200,2500));
-        }
+        }*/
 
 
 
-        submitOrder(10);*/
+//        submitOrder(10);
 
 //        login();
+        /*System.out.println((DateUtils.parseDate("2017-03-17 12:00:00", "yyyy-MM-dd HH:mm:ss").getTime() - new Date().getTime()) / 1000);
+        isRedBagCanPlay("10237");
 
-        /*while (true) {
+        while (true) {
             Date now = new Date();
-            if (now.after(DateUtils.parseDate("2017-02-23 11:59:55", "yyyy-MM-dd HH:mm:ss"))) {
+            if (now.after(DateUtils.parseDate("2017-03-17 11:59:50", "yyyy-MM-dd HH:mm:ss"))) {
                 if (isRedBagCanPlay("10237")) {
                     break;
                 }
             }
             Thread.sleep(1000);
-        }
-        redBag2(5);*/
+        }*/
 
 
         /*while (true) {
             Date now = new Date();
-            if (now.after(DateUtils.parseDate("2017-02-23 19:59:55", "yyyy-MM-dd HH:mm:ss"))) {
+            if (now.after(DateUtils.parseDate("2017-03-09 19:59:55", "yyyy-MM-dd HH:mm:ss"))) {
                 if (isRedBagCanPlay("10393")) {
                     break;
                 }
             }
             Thread.sleep(1000);
         }
-        redBag3(5);*/
+        redBag3(3);*/
 
-//        redBag1(5, "10477"); // 每日红包
-//        redBag1(5, "10495"); // 张杰
-//        redBag1(5, "10492"); // 何炅
+
+
+
+        redBag1(5, "10477"); // 每日红包
+//        redBag1(5, "10495"); //
+//        redBag1(5, "10492"); //
 
         /*redBag2(1);
         Thread.sleep(1000);
         redBag2(1);*/
 
+//        redBagMangGuo(10, "10228");
+
+//        Thread.sleep(60000);
+//        buyDior(1);
+
+    }
+
+    private static void buyDior(int times) throws Exception {
+        for (int i=0;i<times;i++) {
+            String url = "http://shop.mgtvhd.com/shopM/ShopOrderInfoPay_toSucessOurMoneyNew.do";
+            List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+            paramList.add(new BasicNameValuePair("isAllBuy", "n"));
+            paramList.add(new BasicNameValuePair("payType", "coin"));
+            paramList.add(new BasicNameValuePair("payCount", "1"));
+            paramList.add(new BasicNameValuePair("itemId", "10460"));
+            paramList.add(new BasicNameValuePair("userId", "141255"));
+            HttpClientUtils.postResponse(url, paramList);
+            Thread.sleep(sleepMillisecond(2000, 3000));
+        }
     }
 
     private static void login() throws Exception {
@@ -125,16 +163,46 @@ public class MGTV {
         String url = "http://activity.mgtvhd.com/commonWebM/CommonGameIfWin_getUserGameRecordInfo.do?userId=141255&gameId=" + gameId + "&deviceNumber=869922026733969&pageStart=0&pageSize=20";
         JSONObject baseInfoJSON = HttpClientUtils.postResponse(url, null);
         JSONObject baseInfo = baseInfoJSON.getJSONObject("data");
+        boolean start = true;
         if (baseInfo.getIntValue("redbagCountdown") > 0) {
             System.out.println("红包游戏[" + baseInfo.getJSONObject("gameInfo").getIntValue("id") + "]还没到时间，开始时间[" + baseInfo.getJSONObject("redbagGameInfo").getString("startTime") + "], 剩余[" + baseInfo.getString("redbagCountdown") + "秒]");
-            return false;
+            start = false;
         }
 
         if (baseInfo.getBooleanValue("noRedBag")) {
             System.out.println("红包游戏[" + baseInfo.getJSONObject("gameInfo").getIntValue("id") + "]已经没有红包");
-            return false;
+            start = false;
         }
-        return true;
+        if (!start) {
+            Integer c = baseInfo.getInteger("redbagCountdown");
+            if (c > 0) {
+                Thread.sleep((c)*1000);
+                return isRedBagCanPlay(gameId);
+            }
+        }
+
+        return start;
+    }
+
+    /**
+     * 红包游戏1
+     * 10477,10495,10492
+     *
+     * @param times
+     * @throws Exception
+     */
+    private static void redBagMangGuo(int times, String gameId) throws Exception {
+        if (!isRedBagCanPlay(gameId)) {
+            return;
+        }
+        String url = "http://activity.mgtvhd.com/commonWebM/CommonGameRedbag_dealRedBag.do?userId=141255&gameId=" + gameId + "&deviceNumber=869922026733969&payTypeUser=coin";
+        for (int i = 0; i < times; i++) {
+            JSONObject result = HttpClientUtils.getResponse(url, null);
+            if ("您今天的游戏次数已用完！".equals(result.getString("data"))) {
+                break;
+            }
+            Thread.sleep(sleepMillisecond(1500, 2500));
+        }
     }
 
     /**
@@ -148,6 +216,7 @@ public class MGTV {
         if (!isRedBagCanPlay(gameId)) {
             return;
         }
+        Thread.sleep(1000);
         String url = "http://activity.mgtvhd.com/commonWebM/CommonGameRedbag_dealRedBag.do?userId=141255&gameId=" + gameId + "&deviceNumber=869922026733969&payTypeUser=free";
         for (int i = 0; i < times; i++) {
             JSONObject result = HttpClientUtils.getResponse(url, null);
@@ -168,6 +237,9 @@ public class MGTV {
         String url = "http://activity.mgtvhd.com/commonWebM/CommonGameRedbag_dealRedBag.do?userId=141255&gameId=10237&deviceNumber=869922026733969&payTypeUser=dou";
         for (int i = 0; i < times; i++) {
             JSONObject result = HttpClientUtils.getResponse(url, null);
+            if (result.getBooleanValue("success")) {
+                break;
+            }
             if ("您今天的游戏次数已用完！".equals(result.getString("data")) || "null".equals(result.getString("data"))) {
                 break;
             }
@@ -185,6 +257,9 @@ public class MGTV {
         String url = "http://activity.mgtvhd.com/commonWebM/CommonGameRedbag_dealRedBag.do?userId=141255&gameId=10393&deviceNumber=869922026733969&payTypeUser=dou";
         for (int i = 0; i < times; i++) {
             JSONObject result = HttpClientUtils.getResponse(url, null);
+            if (result.getBooleanValue("success")) {
+                break;
+            }
             if ("您今天的游戏次数已用完！".equals(result.getString("data"))) {
                 break;
             }
@@ -209,6 +284,44 @@ public class MGTV {
     private static int sleepMillisecond(int min, int max) {
         Random random = new Random();
         return random.nextInt(max) % (max - min + 1) + min;
+    }
+
+    /**
+     * 获取商品列表
+     * @return
+     * @throws Exception
+     */
+    private static void submitHuaFei(int times) throws Exception {
+        String url = "http://shop.mgtvhd.com/shopM/ShopItemInfo_selectAppHomeItemList.do";
+        List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+        paramList.add(new BasicNameValuePair("orderType", "applyPersonCountDesc"));
+        paramList.add(new BasicNameValuePair("userId", ""));
+        paramList.add(new BasicNameValuePair("pageStart", "0"));
+        paramList.add(new BasicNameValuePair("pageSize", "100"));
+        JSONObject result = HttpClientUtils.postResponse(url, paramList);
+        JSONArray dataArray = result.getJSONArray("data");
+        for (int i = 0; i < dataArray.size(); i++) {
+            JSONObject data = dataArray.getJSONObject(i);
+            String title = data.getString("title");
+            if ("新手20元话费卡抢先体验".equals(title)) {
+                String itemId = data.getString("id");
+                for (int m=0;m<times;m++) {
+                    submit(itemId);
+                    Thread.sleep(sleepMillisecond(1000, 3000));
+                }
+            }
+        }
+    }
+
+    private static void submit(String itemId) throws Exception {
+        String url = "http://shop.mgtvhd.com/shopM/ShopOrderInfoPay_toSucessOurMoneyNew.do";
+        List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+        paramList.add(new BasicNameValuePair("isAllBuy", "n"));
+        paramList.add(new BasicNameValuePair("payType", "dou"));
+        paramList.add(new BasicNameValuePair("payCount", "10"));
+        paramList.add(new BasicNameValuePair("itemId", itemId));
+        paramList.add(new BasicNameValuePair("userId", "141255"));
+        HttpClientUtils.postResponse(url, paramList);
     }
 
 }
