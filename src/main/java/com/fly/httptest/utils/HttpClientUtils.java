b1c56ab6fcdcf6fc9202d7c67c5c63ceddb1f32a
jackdaifei <jackdaifei@126.com>
2017-11-13 23:02:01 +0800

    jd

--- src/main/java/com/fly/httptest/utils/HttpClientUtils.java
+++ src/main/java/com/fly/httptest/utils/HttpClientUtils.java
@@ -4,19 +4,25 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
+import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.params.ClientPNames;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
 import org.apache.http.util.EntityUtils;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Administrator on 2017/2/1.
@@ -184,9 +190,14 @@ public class HttpClientUtils {
     }
 
 
-    public static String get(String url, Header[] headers) throws Exception {
+    public static void get(String url, Header[] headers, Map<String, String> cookieMap, String skuId) throws Exception {
         try {
+            if (!url.startsWith("http")) {
+                url = "https:" + url;
+            }
+            System.out.println("url:------------------------------------------> " + url);
             HttpGet httpGet = new HttpGet(url);
+
             if (ArrayUtils.isNotEmpty(headers)) {
                 httpGet.setHeaders(headers);
             }
@@ -195,16 +206,106 @@ public class HttpClientUtils {
                     .setConnectTimeout(5000)
                     .setConnectionRequestTimeout(5000)
                     .setStaleConnectionCheckEnabled(true)
+                    .setRedirectsEnabled(false)
                     .build();
             CloseableHttpClient client = HttpClients.custom().setDefaultRequestConfig(defaultRequestConfig).build();
 
             CloseableHttpResponse response = client.execute(httpGet);
-//            response
+            Header[] cookies = response.getHeaders("Set-Cookie");
+            for (Header cookie : cookies) {
+                String c = cookie.getValue();
+                String name = c.substring(0, c.indexOf("="));
+                String value = c.substring(c.indexOf("=") + 1, c.indexOf(";"));
+                cookieMap.put(name, value);
+            }
 
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_MOVED_PERMANENTLY ||
+                    response.getStatusLine().getStatusCode() == HttpStatus.SC_MOVED_TEMPORARILY) {
+                Header responseHeader = response.getFirstHeader("Location");
+                String u = responseHeader.getValue();
+                System.out.println("Location:------------------------------------------> " + u);
+
+                Header[] newHeader = new Header[headers.length];
+                for (int i=0;i<headers.length;i++) {
+                    Header header = headers[i];
+                    if (header.getName().equalsIgnoreCase("Host") && StringUtils.isNotBlank(u)) {
+                        String tempU = u.split("//")[1];
+                        newHeader[i] = new BasicHeader("Host", tempU.substring(0, tempU.indexOf("/")));
+                    } else if (header.getName().equalsIgnoreCase("Cookie")) {
+                        newHeader[i] = new BasicHeader("Cookie", cookieString(cookieMap));
+                    } else {
+                        newHeader[i] = header;
+                    }
+                }
+                get(u, newHeader, cookieMap, skuId);
+            } else {
+                String responseStr = EntityUtils.toString(response.getEntity());
+                System.out.println("-------------------------HTML-------------------------");
+                System.out.println("------------------------------------------------------");
+                System.out.println(responseStr);
+                System.out.println("------------------------------------------------------");
+                System.out.println("-------------------------HTML-------------------------");
+
+                String submitUrl = "https://marathon.jd.com/seckill/submitOrder.action?skuId=" + skuId + "&vid=";
+                Header[] submitHeaders = new Header[]{
+                        new BasicHeader("Host", "marathon.jd.com"),
+                        new BasicHeader("Connection", "keep-alive"),
+                        new BasicHeader("Accept", "application/json, text/javascript, */*; q=0.01"),
+                        new BasicHeader("Origin", "https://marathon.jd.com"),
+                        new BasicHeader("X-Requested-With", "XMLHttpRequest"),
+                        new BasicHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"),
+                        new BasicHeader("Content-Type", "application/x-www-form-urlencoded"),
+                        new BasicHeader("Referer", url),
+                        new BasicHeader("Accept-Encoding", "gzip, deflate, br"),
+                        new BasicHeader("Accept-Language", "zh-CN,zh;q=0.8"),
+                        new BasicHeader("Cookie", cookieString(cookieMap))
+                };
+                List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+                paramList.add(new BasicNameValuePair("orderParam.name", "代飞"));
+                paramList.add(new BasicNameValuePair("orderParam.addressDetail", "中柏大道蓝光金悦派(新怡华庭南50米)蓝光金悦派"));
+                paramList.add(new BasicNameValuePair("orderParam.mobile", "159****3559"));
+                paramList.add(new BasicNameValuePair("orderParam.email", ""));
+                paramList.add(new BasicNameValuePair("orderParam.provinceId", "22"));
+                paramList.add(new BasicNameValuePair("orderParam.cityId", "1930"));
+                paramList.add(new BasicNameValuePair("orderParam.countyId", "50949"));
+                paramList.add(new BasicNameValuePair("orderParam.townId", "52154"));
+                paramList.add(new BasicNameValuePair("orderParam.paymentType", "4"));
+                paramList.add(new BasicNameValuePair("orderParam.password", ""));
+                paramList.add(new BasicNameValuePair("orderParam.invoiceTitle", "4"));
+                paramList.add(new BasicNameValuePair("orderParam.invoiceContent", "1"));
+                paramList.add(new BasicNameValuePair("orderParam.invoiceCompanyName", ""));
+                paramList.add(new BasicNameValuePair("orderParam.invoiceTaxpayerNO", ""));
+                paramList.add(new BasicNameValuePair("orderParam.usualAddressId", "138742315"));
+                paramList.add(new BasicNameValuePair("skuId", skuId));
+                paramList.add(new BasicNameValuePair("num", "1"));
+                paramList.add(new BasicNameValuePair("orderParam.provinceName", "四川"));
+                paramList.add(new BasicNameValuePair("orderParam.cityName", "成都市"));
+                paramList.add(new BasicNameValuePair("orderParam.countyName", "高新区"));
+                paramList.add(new BasicNameValuePair("orderParam.townName", "中和镇"));
+                paramList.add(new BasicNameValuePair("orderParam.codTimeType", "3"));
+                paramList.add(new BasicNameValuePair("orderParam.mobileKey", "8af2df2e7cb1cb50f0e62eb538900ae6"));
+                paramList.add(new BasicNameValuePair("eid", ""));
+                paramList.add(new BasicNameValuePair("fp", ""));
+
+                System.out.println("----------------------------------------submit response -----------------------------------------");
+                System.out.println(HttpClientUtils.postResponseString(submitUrl, paramList, submitHeaders));
+                System.out.println("---------------------------submit success !!! ----------------------------------");
+
+//                HttpClientUtils.postResponseString(submitUrl, submitHeaders);
+            }
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
-        return null;
+    }
+
+    private static String cookieString(Map<String, String> cookieMap) {
+        StringBuilder sb = new StringBuilder();
+        Set<Map.Entry<String, String>> entrySet = cookieMap.entrySet();
+        for (Map.Entry<String, String> map : entrySet) {
+            sb.append(map.getKey()).append("=").append(map.getValue()).append("; ");
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
     }
 
     public static String getResponseString(String url, Header[] headers) throws Exception {
